392340d813df1ae10fb3fc768ab3583bc7f65b21
jayaprakash1993 <jpksastra@gmail.com>
2022-11-23 14:50:13 +0530

    A added code

--- src/org/test/Example.java
+++ src/org/test/Example.java
@@ -2,10 +2,18 @@ package org.test;
 
 public class Example {
 	
+	private void m1() {
+		System.out.println("A to m1");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		int a =100;
+		System.out.println(a);
 		
+		
+		System.out.println("Good Afternoon");
 		
 	}
 
